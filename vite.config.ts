050f7f5a97bdeab47e4549e3c17db4cc877ee69a
Димаков Андрей Владиславович <Dimakov_AV@sever.local>
2026-02-10 13:33:37 +0700

    Настройка окружения

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "./public"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/vars" as *;`,
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
